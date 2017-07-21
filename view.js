3926afa1119f769042f6102ce06411d254bc2772
Magnus Dahlstrand <magnus@mdahlstrand.com>
2017-07-21 02:36:49 +0100

    `render` and `component` make up `view`

---
+++ view.js
@@ -0,0 +1,44 @@
+var morph = require('morphdom');
+
+function ensureChildNodes(root) {
+	if(root && root.childNodes && root.childNodes.length) {
+		return Promise.resolve(root);
+	}
+	return Promise.reject();
+}
+
+function firstChild(root) {
+	return ensureChildNodes(root)
+	.then(() => Promise.resolve(root.childNodes[0]));
+}
+
+function component($template, events=null) {
+	// console.log('component', $template);
+	if(!document) {
+		return $template;
+	}
+	return $template
+	.then(firstChild)
+	.then(el => {
+		if(events) {
+			// TODO: We should unbind
+			on(el, events);
+		}
+		return Promise.resolve(el);
+	})
+	.catch(() => {
+		// no child nodes
+	})
+}
+
+// Rendering
+function render(rootEl, $ui, state) {
+	$ui(state).then(content => {
+		morph(rootEl, content)
+	})
+}
+
+module.exports = {
+	component,
+	render
+};
